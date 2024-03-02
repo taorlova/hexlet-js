4c92561f06b2c54b1a464bd9c1be7a6dd654d711
Tatiana Orlova <orlovatatyanaa@gmail.com>
2024-03-02 22:58:30 +0300

    fix some errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import _ from "lodash";
-const hello = () => {
-	console.log("Hello, Hexlet!");
-	console.log(_.last(["one", "two"]));
-}
+import _ from 'lodash';
 
-export default hello; 
+const hello = () => {
+  console.log('Hello, Hexlet!');
+  console.log(_.last(['one', 'two']));
+};
+
+export default hello;
